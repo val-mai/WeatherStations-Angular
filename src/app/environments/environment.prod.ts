9be2a672134baede67ea03d1cd47f49ed9cb1c4a
Valerio Maiolini <69992843+val-mai@users.noreply.github.com>
2025-01-14 14:21:40 +0100

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: "http://meteo.api.valmai.org//api/v1",
+  apiURL: "http://meteo.api.valmai.org/api/v1",
   localStorage: "mm-storage",
   comingSoon: false
 };
